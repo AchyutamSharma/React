3a8fa02462567753e19a5c079e61de303cb444ec
= <achyutamgrd@gmail.com>
2024-05-01 17:13:48 +0530

    add new page first time

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
+import Chai from './myCode'
 
 
 function App() {
 
 
   return (
+    // this is called fragement
+    <>
     <h1> <a href="https://akshutam-portfolio.vercel.app/">Akshutam dev.</a></h1>
+    <Chai/>
+    </>
   )
 }
 
---
+++ 01vite_react/src/myCode.jsx
@@ -0,0 +1,11 @@
+
+
+function Chai(){
+    return (
+        
+        <h1>Hello, This is my first react page.</h1>
+
+    )
+}
+
+export default Chai
