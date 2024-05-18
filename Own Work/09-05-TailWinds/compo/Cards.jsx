d9e460a63f0d289e4a441d7c772a26d8dfd298d1
= <achyutamgrd@gmail.com>
2024-05-18 20:01:22 +0530

    complete

--- Own Work/09-05-TailWinds/compo/Cards.jsx
+++ Own Work/09-05-TailWinds/compo/Cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-function Cards() {
+function Cards({name,age,dob,about,btntxt ="View Profile"}) {
+
+
   return (
     <div className="relative h-[400px] w-[300px] rounded-md">
   <img
@@ -10,13 +12,16 @@ function Cards() {
   />
   <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
   <div className="absolute bottom-4 left-4 text-left">
-    <h1 className="text-lg font-semibold text-white">Delba</h1>
-    <p className="mt-2 text-sm text-gray-300">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
+    <h1 className="text-lg font-semibold text-white"><span className='text-2xl'>{name}</span></h1>
+    <p className=" text-sm text-gray-300">
+      <span className='text-md text-black underline underline-offset-2 font-bold'>Age -</span>{age}
+      <br />
+      <span className='text-md text-black underline underline-offset-2 font-bold'>D.O.B :</span> {dob}
+      <br />
+      <span className='text-md text-black underline underline-offset-2 font-bold'>About:</span> {about}
     </p>
     <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
+      {btntxt} →
     </button>
   </div>
 </div>
--- Own Work/09-05-TailWinds/src/App.jsx
+++ Own Work/09-05-TailWinds/src/App.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Cards from '../compo/Cards'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let myObj1 = {
+    name: "Akshutam",
+    dob:"12/08/2001",
+    about:`Hey, I am Akshutam from Bangalore,Recently completed my Bachelor degree`,
+  }
+  let myObj2={
+    name:"Puneet",
+    dob:"12/04/2003",
+    about:`Hey, I am Punnet from Bangalore,Recently completed my Bachelor degree`,
+  }
+  let myAge = [23,21]
   return (
     <>
     <h1 className="text-3xl font-bold bg-blue-800 text-black p-6 rounded-2xl">Tailwind</h1>
     <div className="flex-wrap flex gap-5 m-4 p-4 justify-center items-center">
-    <Cards/>
-    <Cards/>
-    <Cards/>
-    <Cards/>
-    <Cards/>
-    <Cards/>
+    <Cards name ={myObj1.name} dob={myObj1.dob} about={myObj1.about} age={myAge[0]} btntxt = "Click Me"/>
+    <Cards name ={myObj2.name} dob={myObj2.dob} about={myObj2.about} age={myAge[1]} />
     </div>
     
     </>
